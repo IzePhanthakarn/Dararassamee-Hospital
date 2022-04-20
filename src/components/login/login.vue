3dcde5a2d02fb99b23c0282744053b96abf06a4d
nikepitchaya <103920925+nikepitchaya@users.noreply.github.com>
2022-04-20 17:13:11 +0700

    login layout complete

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -5,26 +5,31 @@
         <img src="dara-logo.png" alt="logo-dara" width="180" height="160" />
       </div>
       <div class="identity-number">
+        <h2 class="label-input">เลขบัตรประจำตัวประชาชน</h2>
         <input
           class="text-box"
           type="text"
           placeholder="กรุณาใส่เลขบัตรประชาชน"
         />
       </div>
-      <div class="identity-number">
+      <div class="password">
+        <h2 class="label-input">รหัสผ่าน</h2>
         <input
           class="text-box"
           type="password"
           placeholder="กรุณาใส่รหัสผ่าน"
         />
       </div>
+      <div class="extension">
+        <div class="remember">
+          <input type="checkbox" class="check-box" />
+          <p>จดจำฉัน</p>
+        </div>
+        <button type="button" class="forgot-button">ลืมรหัสผ่าน</button>
+      </div>
       <div class="content-button">
-        <div class="confirm-button">
-          <button class="button-box">สมัครสมาชิก</button>
-        </div>
-        <div class="register-button">
-          <button class="button-box">ยืนยัน</button>
-        </div>
+        <button type="button" class="register-button">สมัครสมาชิก</button>
+        <button type="button" class="confirm-button">เข้าสู่ระบบ</button>
       </div>
     </div>
   </form>
@@ -36,5 +41,4 @@ export default {
   name: "login",
 };
 </script>
-
 <style></style>
