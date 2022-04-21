0e53a1ecfef857833e30deffa7cda9aa684ac24a
izephanthakarn <izephanthakarn@hotmail.com>
2022-04-21 15:27:21 +0700

    add profile page add logout button at navbar

--- src/components/mainpage/navbar.vue
+++ src/components/mainpage/navbar.vue
@@ -17,17 +17,20 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ms-auto">
-          <li class="nav-item1 mx-4">
+          <li class="nav-item mx-4">
             <a class="nav-link" aria-current="page" href="#">หน้าหลัก</a>
           </li>
-          <li class="nav-item1 mx-4">
+          <li class="nav-item mx-4">
             <a class="nav-link" href="#">ข้อมูล</a>
           </li>
-          <li class="nav-item1 mx-4">
+          <li class="nav-item mx-4">
             <a class="nav-link" href="#">ประวัติ</a>
           </li>
-          <li class="nav-item1 ms-4">
+          <li class="nav-item mx-4">
             <a class="nav-link" href="#">ติดต่อ</a>
+          </li>
+          <li class="nav-item ms-4 logout-bg">
+            <button class="logout-btn" href="#">ออกจากระบบ</button>
           </li>
         </ul>
       </div>
@@ -54,8 +57,51 @@ export default {
 .nav-link {
   font-size: 20px;
 }
-.nav-link:hover {
+/* .logout-bg {
+  display: flex;
+  align-items: center;
+  background: #a54b72;
+  border-radius: 5px;
+  height: 40px;
+  width: 140px;
+  
+  transition: all .3s;
+}
+.logout-bg:hover{
+  transform: scale(1.05);
+}
+
+.logout-btn {
   color: #fff;
+  text-decoration: none;
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  font-size: 20px;
+  padding-bottom: 3px;
+} */
+.logout-bg{
+  /* height: 50px; */
+  margin: 4px 0;
+}
+button.logout-btn{
+  width: 140px;
+  height: 35px;
+  padding-bottom: 5px;
+  color: #fff;
+  font-size: 20px;
+  border: none;
+  border-radius: 5px;
+  background-color: #a54b72;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  transition: all .3s;
+}
+button.logout-btn:hover{
+  transform: scale(1.05);
+}
+button.logout-btn:active{
+  transform: scale(.95);
 }
 /* Small devices (landscape phones, 576px and up) */
 @media (min-width: 576px) {
