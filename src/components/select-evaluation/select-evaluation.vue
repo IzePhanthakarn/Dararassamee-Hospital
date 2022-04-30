a5f15b321c1ac2c0a6e329efc05b14b769dd4177
Ize Phanthakarn <izephanthakarn@hotmail.com>
2022-04-30 14:12:29 +0700

    add 3 select button

---
+++ src/components/select-evaluation/select-evaluation.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="selector-title container-fluid">
+    <fa :icon="['fas', 'book-medical']" class="selector-icon" />
+    <h1>แบบประเมิน</h1>
+  </div>
+  <div class="body-box container-fluid">
+    <div class="button-area">
+      <div class="button-content">
+        <button>แบบประเมินสุขภาพจิต(Q5)</button>
+      </div>
+      <div class="button-content">
+        <button>แบบประเมินโรคซึมเศร้า(Q9)</button>
+      </div>
+      <div class="button-content">
+        <button>แบบประเมินการฆ่าตัวตาย(Q8)</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+@import url("./select-evaluation.css");
+</style>
